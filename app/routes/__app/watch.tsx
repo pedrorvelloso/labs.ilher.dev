4149efd8f87c596d7053b47a4bd7f0b820cb2e78
Pedro Reis Velloso dos Santos <pedro_reis14@hotmail.com>
2022-03-20 00:39:01 -0300

    add headers to watch page

--- app/routes/__app/watch.tsx
+++ app/routes/__app/watch.tsx
@@ -1,14 +1,18 @@
 import type { LoaderFunction } from 'remix'
 import { json, useLoaderData } from 'remix'
-import { GetWatchQuery } from '~/generated/graphql'
+import type { GetWatchQuery } from '~/generated/graphql'
 
 import { getWatch } from '~/server/cms/graphcms.server'
+
+import { getHeaders, Swr } from '~/utils/headers'
+
 import { WatchList } from '~/ui/compositions/watch-list'
-import { Swr } from '~/utils/headers'
 
 type WatchPageLoaderData = {
   links: GetWatchQuery['links']
 }
+
+export const headers = getHeaders
 
 export const loader: LoaderFunction = async () => {
   const links = await getWatch()
